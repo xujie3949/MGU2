ac67d4e98d1eb28aa74f52334c9a04fa61ba3813
xujie3949 <xujie3949@navinfo.com>
2018-01-03 10:31:51 +0800

    服务暂时修改为webapp的服务地址,以实现登录

--- src/services/service.js
+++ src/services/service.js
@@ -4,83 +4,86 @@ import userStore from 'Stores/userStore';
 import mockData from 'Services/mockData';
 
 class Service {
-  axiosInstance = null;
-  cancelSource = null;
+    axiosInstance = null;
+    cancelSource = null;
 
-  constructor() {
-      this.axiosInstance = this.createAxiosInstance();
-      this.cancelSource = new Map();
-  }
+    constructor() {
+        this.axiosInstance = this.createAxiosInstance();
+        this.cancelSource = new Map();
+    }
 
-  createAxiosInstance() {
-      const axiosInstance = axios.create({
-          baseURL: 'http://192.168.4.129/service/mapspotter',
-          timeout: 10000,
-      });
+    createAxiosInstance() {
+        const axiosInstance = axios.create({
+                                               baseURL: 'http://fs-road.navinfo.com/dev/trunk/service',
+                                               timeout: 10000,
+                                           });
 
-      this.addInterceptor(axiosInstance);
+        this.addInterceptor(axiosInstance);
 
-      return axiosInstance;
-  }
+        return axiosInstance;
+    }
 
-  addInterceptor(axiosInstance) {
-      axiosInstance.interceptors.request.use(this.addCancelToken);
-      axiosInstance.interceptors.request.use(this.addTokenToParams);
-      axiosInstance.interceptors.response.use(this.removeCancelToken);
-  }
+    addInterceptor(axiosInstance) {
+        axiosInstance.interceptors.request.use(this.addCancelToken);
+        axiosInstance.interceptors.request.use(this.addTokenToParams);
+        axiosInstance.interceptors.response.use(this.removeCancelToken);
+    }
 
-  addCancelToken = config => {
-      const source = axios.CancelToken.source();
-      this.cancelSource.set(source.token, source);
-      config.cancelToken = source.token;
-      return config;
-  };
+    addCancelToken = config => {
+        const source = axios.CancelToken.source();
+        this.cancelSource.set(source.token, source);
+        config.cancelToken = source.token;
+        return config;
+    };
 
-  addTokenToParams = config => {
-      if (!config.url.endsWith('/user/login')) {
-          if (!config.params) {
-              config.params = new URLSearchParams();
-          }
-          if (userStore.token) {
-              config.params.append('access_token', userStore.token);
-          } else {
-              config.params.append('access_token', userStore.publicToken);
-          }
-      }
-      return config;
-  };
+    addTokenToParams = config => {
+        if (!config.url.endsWith('/user/login')) {
+            if (!config.params) {
+                config.params = new URLSearchParams();
+            }
+            if (userStore.token) {
+                config.params.append('access_token', userStore.token);
+            } else {
+                config.params.append('access_token', userStore.publicToken);
+            }
+        }
+        return config;
+    };
 
-  removeCancelToken = response => {
-      if (this.cancelSource.has(response.config.cancelToken)) {
-          this.cancelSource.delete(response.config.cancelToken);
-      }
-      return response;
-  };
+    removeCancelToken = response => {
+        if (this.cancelSource.has(response.config.cancelToken)) {
+            this.cancelSource.delete(response.config.cancelToken);
+        }
+        return response;
+    };
 
-  cancelRequest() {
-      for (const [key, value] of this.cancelSource) {
-          value.cancel();
-      }
+    cancelRequest() {
+        for (const [key, value] of this.cancelSource) {
+            value.cancel();
+        }
 
-      this.cancelSource.clear();
-  }
+        this.cancelSource.clear();
+    }
 
-  handleTokenError(data) {
-      if (data.errcode === -100) {
-          appStore.setError(data.errmsg);
-          return true;
-      }
-      return false;
-  }
+    handleTokenError(data) {
+        if (data.errcode === -100) {
+            appStore.setError(data.errmsg);
+            return true;
+        }
+        return false;
+    }
 
-  async login(username, password) {
-      const params = new URLSearchParams();
-      params.append('username', username);
-      params.append('password', password);
-      // const response = await this.axiosInstance.get('user/login', { params: params });
-      // return response.data;
-      return mockData.loginSuccess;
-  }
+    async login(username, password) {
+        const params = new URLSearchParams();
+        params.append('parameter', JSON.stringify(
+            {
+                userNickName: 'hanxuesong01664',
+                userPassword: '016640',
+            },
+        ));
+        const response = await this.axiosInstance.get('man/userInfo/login', { params: params });
+        return response.data;
+    }
 }
 
 const service = new Service();
