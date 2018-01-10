3cae4e706a243553989f90e9f59ca58f6e6898c0
xujie3949 <xujie3949@navinfo.com>
2018-01-10 21:30:03 +0800

    添加leftPanelStore和rightPanelStore

--- src/stores/stores.js
+++ src/stores/stores.js
@@ -4,6 +4,8 @@ import loadingStore from 'Stores/loadingStore';
 import loginStore from 'Stores/loginStore';
 import menuBarStore from 'Stores/menuBarStore';
 import toolBarStore from 'Stores/toolBarStore';
+import leftPanelStore from 'Stores/leftPanelStore';
+import rightPanelStore from 'Stores/rightPanelStore';
 
 const stores = {
     appStore: appStore,
@@ -12,6 +14,8 @@ const stores = {
     loginStore: loginStore,
     menuBarStore: menuBarStore,
     toolBarStore: toolBarStore,
+    leftPanelStore: leftPanelStore,
+    rightPanelStore: rightPanelStore,
 };
 
 export default stores;
