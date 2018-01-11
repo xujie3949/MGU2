e227cec319f736d1ca9eff7462ff451412b27e09
xujie3949 <xujie3949@navinfo.com>
2018-01-11 11:30:15 +0800

    完成TrajectoryPlaybackCommand功能

--- src/business/command/TrajectoryPlaybackCommand.js
+++ src/business/command/TrajectoryPlaybackCommand.js
@@ -1,4 +1,5 @@
 import navinfo from 'Navinfo';
+import stores from 'Stores/stores';
 
 class Command extends navinfo.framework.command.Command {
     constructor() {
@@ -12,7 +13,18 @@ class Command extends navinfo.framework.command.Command {
 
     execute() {
         const editControlFactory = navinfo.framework.editControl.EditControlFactory.getInstance();
+        const editControl = editControlFactory.getEditControl(
+            this.key,
+            {
+                map: stores.mapStore.map,
+            },
+        );
 
+        if (!editControl) {
+            throw new Error(`命令未实现:${this.name}`);
+        }
+
+        editControl.run();
     }
 
     canExecute() {
