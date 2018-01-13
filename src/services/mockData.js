e684990e282f302045e87222e18d5220d647e0d1
xujie3949 <xujie3949@navinfo.com>
2018-01-13 12:02:34 +0800

    添加getTrajectoryList接口模拟数据

--- src/services/mockData.js
+++ src/services/mockData.js
@@ -12,9 +12,26 @@ const loginFail = {
     data: null,
 };
 
+const trajectoryListSuccess = {
+    code: 0,
+    message: 'success',
+    data: [
+        { collectUser: 'LGW00', plateform: '01', utcdate: '161223' },
+        { collectUser: 'LGW00', plateform: '01', utcdate: '161226' },
+        { collectUser: 'LGW00', plateform: '01', utcdate: '161123' },
+        { collectUser: 'LGW00', plateform: '01', utcdate: '161208' },
+        { collectUser: 'LGW00', plateform: '01', utcdate: '161207' },
+        { collectUser: 'LGW00', plateform: '01', utcdate: '160905' },
+        { collectUser: 'LGW00', plateform: '01', utcdate: '160903' },
+        { collectUser: 'LGW00', plateform: '01', utcdate: '161124' },
+        { collectUser: 'LGW00', plateform: '01', utcdate: '161219' },
+    ],
+};
+
 const mockData = {
     loginSuccess: loginSuccess,
     loginFail: loginFail,
+    trajectoryListSuccess: trajectoryListSuccess,
 };
 
 export default mockData;
