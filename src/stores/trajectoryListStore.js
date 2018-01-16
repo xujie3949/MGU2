4f0c4ef13cd8a9c852294a0bcc7425b4e9d7b1f2
xujie3949 <xujie3949@navinfo.com>
2018-01-16 21:13:42 +0800

    trajectoryListStore添加查询单条轨迹点接口并处理相关反馈绘制

--- src/stores/trajectoryListStore.js
+++ src/stores/trajectoryListStore.js
@@ -5,7 +5,7 @@ import {
 } from 'mobx';
 import { Modal } from 'antd';
 
-
+import navinfo from 'Navinfo';
 import service from 'Services/service';
 import delay from 'Utils/delay';
 import User from 'Models/User';
@@ -14,7 +14,8 @@ import userStore from 'Stores/userStore';
 
 class TrajectoryListStore {
     @observable.ref data;
-    @observable.ref selectedIndex;
+    @observable selectedIndex;
+    feedback = null;
 
     constructor() {
         this.data = [];
@@ -31,6 +32,14 @@ class TrajectoryListStore {
         this.selectedIndex = value;
     }
 
+    @action
+    setTrajectoryPoints(index, value) {
+        if (index < 0 || index > this.data.length - 1) {
+            return;
+        }
+        this.data[index].points = value;
+    }
+
     @computed
     get selected() {
         if (this.selectedIndex < 0 || this.selectedIndex > this.data.length - 1) {
@@ -39,13 +48,13 @@ class TrajectoryListStore {
         return this.data[this.selectedIndex];
     }
 
-    async fetchData() {
+    async fetchData(northWest, southEast) {
         try {
             loadingStore.show();
 
             const start = Date.now();
 
-            const data = await service.getTrajectoryList('hanxuesong01664', '016640');
+            const data = await service.getTrajectoryList(northWest, southEast);
 
             const diff = Date.now() - start;
 
@@ -66,9 +75,74 @@ class TrajectoryListStore {
         }
     }
 
-    logout() {
-        userStore.setUser(null);
-        userStore.clearUserInfo();
+    async fetchSelectedTrajectoryPoints() {
+        try {
+            loadingStore.show();
+
+            const start = Date.now();
+
+            const data = await service.getTrajectoryDetail(this.selected);
+
+            const diff = Date.now() - start;
+
+            if (diff < 500) {
+                await delay(500 - diff);
+            }
+
+            loadingStore.close();
+
+            if (data.code === 0) {
+                this.setTrajectoryPoints(this.selectedIndex, data.data);
+            } else {
+                Modal.error(data.message);
+            }
+        } catch (err) {
+            loadingStore.close();
+            Modal.error(err.message);
+        }
+    }
+
+    updateTrajectoryFeedback() {
+        const feedbackController = navinfo.mapApi.feedback.FeedbackController.getInstance();
+
+        if (!this.feedback) {
+            this.feedback = new navinfo.mapApi.feedback.Feedback();
+            feedbackController.add(this.feedback);
+        }
+
+        this.feedback.clear();
+
+        if (this.selected) {
+            const points = this.selected.points;
+            const symbolData = {
+                type: 'CircleMarkerSymbol',
+                radius: 3,
+                color: 'blue',
+            };
+            const geometryFactory = navinfo.geometry.GeometryFactory.getInstance();
+            const symbolFactory = navinfo.symbol.SymbolFactory.getInstance();
+            const symbol = symbolFactory.createSymbol(symbolData);
+            points.forEach(item => {
+                const geometry = {
+                    type: 'Point',
+                    coordinates: [item.lon, item.lat],
+                };
+                this.feedback.add(geometry, symbol);
+            });
+        }
+
+        feedbackController.refresh();
+    }
+
+    clearFeedback() {
+        if (!this.feedback) {
+            return;
+        }
+
+        this.feedback.clear();
+        const feedbackController = navinfo.mapApi.feedback.FeedbackController.getInstance();
+        feedbackController.del(this.feedback);
+        feedbackController.refresh();
     }
 }
 
