b580f83571c05d8cb55b46d13e1f3d3966b8ccf9
xujie3949 <xujie3949@navinfo.com>
2018-01-16 21:14:46 +0800

    添加轨迹回放流程

--- src/business/editControl/TrajectoryPlaybackControl.js
+++ src/business/editControl/TrajectoryPlaybackControl.js
@@ -1,15 +1,17 @@
+import React from 'react';
+
 import navinfo from 'Navinfo';
+import stores from 'Stores/stores';
+import service from 'Services/service';
 
 /**
  * Created by xujie3949 on 2016/12/28.
  */
 
 class TrajectoryPlaybackControl extends navinfo.framework.editControl.EditControl {
-    constructor(map, options) {
-        super(map, options);
-
-        this.geoLiveType = options.geoLiveType;
-        this.topoEditor = this.topoEditFactory.getTopoEditor('create', this.geoLiveType, { map: this.map });
+    constructor(options) {
+        super(options);
+        this.feedback = null;
     }
 
     run() {
@@ -17,48 +19,84 @@ class TrajectoryPlaybackControl extends navinfo.framework.editControl.EditContro
             return false;
         }
 
-        const editResult = this.topoEditor.getCreateEditResult();
-        this.shapeEditor.start(editResult, this.onToolFinish);
+        stores.leftPanelStore.open();
+        stores.rightPanelStore.close();
+
+        this.toolController.resetCurrentTool('TrajectoryPlaybackTool', this.onFinish, null);
 
         return true;
     }
 
-    onToolFinish(editResult) {
-        if (!this.precheck(editResult)) {
+    abort() {
+        super.abort();
+        this.clearFeedback();
+    }
+
+    async onFinish(point) {
+        const detail = await this.fetchPointDetail(point);
+        this.updateTrajectoryFeedback(detail);
+    }
+
+    updateTrajectoryFeedback(detail) {
+        const feedbackController = navinfo.mapApi.feedback.FeedbackController.getInstance();
+
+        if (!this.feedback) {
+            this.feedback = new navinfo.mapApi.feedback.Feedback();
+            feedbackController.add(this.feedback);
+        }
+
+        this.feedback.clear();
+
+        if (detail) {
+            const geometry = {
+                type: 'Point',
+                coordinates: [detail.lon, detail.lat],
+            };
+            const symbol = this.symbolFactory.getSymbol('trajectory_currentPoint');
+            const cloneSymbol = navinfo.common.Util.clone(symbol);
+            cloneSymbol.angle = 0;
+            this.feedback.add(geometry, symbol);
+        }
+
+        feedbackController.refresh();
+    }
+
+    clearFeedback() {
+        if (!this.feedback) {
             return;
         }
 
-        this.toolController.pause();
-
-        this.topoEditor
-            .create(editResult)
-            .then(this.onSuccess)
-            .catch(this.onFail);
+        this.feedback.clear();
+        const feedbackController = navinfo.mapApi.feedback.FeedbackController.getInstance();
+        feedbackController.del(this.feedback);
+        feedbackController.refresh();
     }
 
-    onSuccess(res) {
-        this.toolController.continue();
+    async fetchPointDetail(point) {
+        try {
+            stores.loadingStore.show();
 
-        this.shapeEditor.stop();
+            const start = Date.now();
 
-        // 根据服务log获取发生变更的要素类型列表
-        const geoLiveTypes = this.getChangedGeoLiveTypes(this.geoLiveType, res.log);
+            const data = await service.getTrajectoryDetail(this.selected);
 
-        // 刷新对应图层
-        this.sceneController.redrawLayerByGeoLiveTypes(geoLiveTypes);
+            const diff = Date.now() - start;
 
-        // 自动选中要素
+            if (diff < 500) {
+                await navinfo.common.Util.delay(500 - diff);
+            }
 
-        super.onSuccess(res);
+            stores.loadingStore.close();
 
-        // 重新执行流程方便连续创建
-        this.run();
-    }
-
-    onFail(err) {
-        this.toolController.continue();
-
-        super.onFail(err);
+            if (data.code === 0) {
+                stores.imageViewerStore.setSrc(`data:image/jpeg;base64,${data.data.photoStr}`);
+            } else {
+                stores.modalStore.error(data.message);
+            }
+        } catch (err) {
+            stores.loadingStore.close();
+            stores.modalStore.error(err.message);
+        }
     }
 }
 
