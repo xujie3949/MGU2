52b0e1dc811180065fb5dece11640ecfac04632b
xujie3949 <xujie3949@navinfo.com>
2018-01-16 21:18:51 +0800

    修改mockdata中轨迹列表数据

--- src/services/mockData.js
+++ src/services/mockData.js
@@ -16,15 +16,141 @@ const trajectoryListSuccess = {
     code: 0,
     message: 'success',
     data: [
-        { collectUser: 'LGW00', plateform: '01', utcdate: '161223' },
-        { collectUser: 'LGW00', plateform: '01', utcdate: '161226' },
-        { collectUser: 'LGW00', plateform: '01', utcdate: '161123' },
-        { collectUser: 'LGW00', plateform: '01', utcdate: '161208' },
-        { collectUser: 'LGW00', plateform: '01', utcdate: '161207' },
-        { collectUser: 'LGW00', plateform: '01', utcdate: '160905' },
-        { collectUser: 'LGW00', plateform: '01', utcdate: '160903' },
-        { collectUser: 'LGW00', plateform: '01', utcdate: '161124' },
-        { collectUser: 'LGW00', plateform: '01', utcdate: '161219' },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161224',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161216',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161225',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161210',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161215',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '160902',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161217',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161209',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '160831',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '160830',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '160828',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '160829',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '160907',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161213',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161214',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161129',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161125',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161128',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161223',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '160921',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161212',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161123',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '160905',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161124',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161218',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '160827',
+        },
+        {
+            plateform: '01',
+            collectUser: 'LGW00',
+            utcdate: '161219',
+        },
     ],
 };
 
