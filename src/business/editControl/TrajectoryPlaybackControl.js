467d790334498618cb05618142ad057a55e72c52
xujie3949 <xujie3949@navinfo.com>
2018-01-18 09:41:28 +0800

    TrajectoryPlaybackControl中去掉关于反馈绘制和服务请求的代码

--- src/business/editControl/TrajectoryPlaybackControl.js
+++ src/business/editControl/TrajectoryPlaybackControl.js
@@ -11,7 +11,6 @@ import service from 'Services/service';
 class TrajectoryPlaybackControl extends navinfo.framework.editControl.EditControl {
     constructor(options) {
         super(options);
-        this.feedback = null;
     }
 
     run() {
@@ -19,7 +18,7 @@ class TrajectoryPlaybackControl extends navinfo.framework.editControl.EditContro
             return false;
         }
 
-        stores.leftPanelStore.open();
+        stores.leftPanelStore.close();
         stores.rightPanelStore.close();
 
         this.toolController.resetCurrentTool('TrajectoryPlaybackTool', this.onFinish, null);
@@ -32,72 +31,11 @@ class TrajectoryPlaybackControl extends navinfo.framework.editControl.EditContro
         this.clearFeedback();
     }
 
-    async onFinish(point) {
-        const detail = await this.fetchPointDetail(point);
-        this.updateTrajectoryFeedback(detail);
-    }
-
-    updateTrajectoryFeedback(detail) {
-        const feedbackController = navinfo.mapApi.feedback.FeedbackController.getInstance();
-
-        if (!this.feedback) {
-            this.feedback = new navinfo.mapApi.feedback.Feedback();
-            feedbackController.add(this.feedback);
-        }
-
-        this.feedback.clear();
-
-        if (detail) {
-            const geometry = {
-                type: 'Point',
-                coordinates: [detail.lon, detail.lat],
-            };
-            const symbol = this.symbolFactory.getSymbol('trajectory_currentPoint');
-            const cloneSymbol = navinfo.common.Util.clone(symbol);
-            cloneSymbol.angle = 0;
-            this.feedback.add(geometry, symbol);
-        }
-
-        feedbackController.refresh();
-    }
-
-    clearFeedback() {
-        if (!this.feedback) {
-            return;
-        }
-
-        this.feedback.clear();
-        const feedbackController = navinfo.mapApi.feedback.FeedbackController.getInstance();
-        feedbackController.del(this.feedback);
-        feedbackController.refresh();
-    }
-
-    async fetchPointDetail(point) {
-        try {
-            stores.loadingStore.show();
-
-            const start = Date.now();
-
-            const data = await service.getTrajectoryDetail(this.selected);
-
-            const diff = Date.now() - start;
-
-            if (diff < 500) {
-                await navinfo.common.Util.delay(500 - diff);
-            }
-
-            stores.loadingStore.close();
-
-            if (data.code === 0) {
-                stores.imageViewerStore.setSrc(`data:image/jpeg;base64,${data.data.photoStr}`);
-            } else {
-                stores.modalStore.error(data.message);
-            }
-        } catch (err) {
-            stores.loadingStore.close();
-            stores.modalStore.error(err.message);
-        }
-    }
+    onFinish = async point => {
+        await point.fetchDetail();
+        stores.imageViewerStore.setIndex(point.id);
+        this.eventController.fire('SelectedTrajectoryPointChanged', null);
+    };
 }
 
 export default TrajectoryPlaybackControl;
