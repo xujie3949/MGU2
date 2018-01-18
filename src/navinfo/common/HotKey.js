cdcc74b1a95b58bb4b24c41eed6538e8591de6ef
xujie3949 <xujie3949@navinfo.com>
2018-01-18 20:47:50 +0800

    修复HotKey不响应问题

--- src/navinfo/common/HotKey.js
+++ src/navinfo/common/HotKey.js
@@ -1,16 +1,24 @@
 import mousetrap from 'mousetrap';
 
 class HotKey {
-    static bind(hotKey, func, ...args) {
-        mousetrap.bind(hotKey, func, args);
+    static bind(hotKey, func, action = 'keyup') {
+        mousetrap.bind(hotKey, func, action);
     }
 
-    static unbind(hotKey, ...args) {
-        mousetrap.unbind(hotKey, args);
+    static unbind(hotKey, action = 'keyup') {
+        mousetrap.unbind(hotKey, action);
     }
 
-    static trigger(hotKey, ...args) {
-        mousetrap.trigger(hotKey, args);
+    static trigger(hotKey, action = 'keyup') {
+        mousetrap.trigger(hotKey, action);
+    }
+
+    static pause(hotKey) {
+        mousetrap.pause(hotKey);
+    }
+
+    static unpause(hotKey) {
+        mousetrap.unpause(hotKey);
     }
 
     static reset(hotKey) {
