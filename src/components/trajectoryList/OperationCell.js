ca43b63a98196c4c3543c319909544c6af1f5a6a
xujie3949 <xujie3949@navinfo.com>
2018-01-18 09:28:50 +0800

    添加OperationCell组件,使用observe包装,使得UI状态能够根据Store状态刷新,并且能够保存被选中的对象,不用在data-属性中记载主键

---
+++ src/components/trajectoryList/OperationCell.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { observer } from 'mobx-react';
+import {
+    Row,
+    Table,
+    Icon,
+    Divider,
+} from 'antd';
+
+import stores from 'Stores/stores';
+import Panel from 'Components/panel/Panel';
+import navinfo from 'Navinfo';
+import style from './styles/style.styl';
+
+const { Column } = Table;
+
+@observer
+export default class OperationCell extends Component {
+    static propTypes = {
+        trajectoryLine: PropTypes.object.isRequired,
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    onLoadClick = async e => {
+        stores.trajectoryListStore.setSelected(this.props.trajectoryLine);
+        await this.props.trajectoryLine.fetchDetail();
+        const center = [
+            stores.trajectoryListStore.selected.points[0].latitude,
+            stores.trajectoryListStore.selected.points[0].longitude,
+        ];
+        stores.mapStore.map.flyTo(center);
+
+        const eventController = navinfo.common.EventController.getInstance();
+        eventController.fire('SelectedTrajectoryLineChanged', null);
+    }
+
+    onPlayClick = async e => {
+        const commandFactory = navinfo.framework.command.CommandFactory.getInstance();
+        const command = commandFactory.getCommand('TrajectoryPlayback');
+        if (command) {
+            command.execute();
+        }
+    }
+
+    render() {
+        const trajectoryLine = this.props.trajectoryLine;
+        let canLoad = false;
+        let canPlay = false;
+        if (!trajectoryLine.points || trajectoryLine.points.length === 0) {
+            canLoad = true;
+            canPlay = false;
+        } else {
+            canLoad = false;
+            canPlay = true;
+        }
+
+        return (
+            <span>
+                <a
+                    disabled={ !canLoad }
+                    onClick={ this.onLoadClick }
+                >
+                    加载
+                </a>
+                <Divider type="vertical"/>
+                <a
+                    disabled={ !canPlay }
+                    onClick={ this.onPlayClick }
+                >
+                    播放
+                </a>
+            </span>
+        );
+    }
+}
